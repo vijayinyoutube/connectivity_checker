260d1d15a78e7f91e9e017c55c80ec95a06943f0
vijayinyoutube <vijaythevirtue@gmail.com>
2022-07-12 23:29:07 +0530

    refactor

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connecctivity Checker'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Check'),
+          onPressed: () => checkConnectivity(context),
+        ),
+      ),
+    );
   }
+
+  Future<void> checkConnectivity(BuildContext context) async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      showDialogBox(context);
+    }
+  }
+
+  showDialogBox(BuildContext context) => showCupertinoDialog<String>(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: const Text('No Connection'),
+          content: const Text('Please check your internet connectivity'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
 }
